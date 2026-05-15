ee921f79e439abc837e7fe92f307a4932cf2248e
douama <sobekande@gmail.com>
2026-05-15 04:56:33 +0000

    Revert "feat(landing): replace UGC video with talking-head skincare creator, remove FeatureBento"
    This reverts commit 04380bcc621e8772f4fbb2d4225e87e912a30302.

--- src/pages/public/Landing.tsx
+++ src/pages/public/Landing.tsx
@@ -1,5 +1,6 @@
 import { Hero } from "../../components/marketing/Hero";
 import { LiveDecisionsStrip } from "../../components/marketing/LiveDecisionsStrip";
+import { FeatureBento } from "../../components/marketing/FeatureBento";
 import { HowItWorks } from "../../components/marketing/HowItWorks";
 import { Pricing } from "../../components/marketing/Pricing";
 import { Testimonials } from "../../components/marketing/Testimonials";
@@ -12,6 +13,7 @@ export function LandingPage() {
     <>
       <Hero />
       <LiveDecisionsStrip />
+      <FeatureBento />
       <HowItWorks />
       <Testimonials />
       <UGCShowcase />
